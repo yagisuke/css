8251fe4351a04f9e5c366380f4fc5c183cc81f8f
yagisuke <yagisuke.0209@gmail.com>
2019-12-12 01:51:38 +0900

    modify focus-within sample

--- src/components/views/styles/focus_within/sample1.tsx
+++ src/components/views/styles/focus_within/sample1.tsx
@@ -8,23 +8,41 @@ type Props = {
 const View: React.FC<Props> = props => {
   const [text, setText] = useState('')
   return (
-    <div className={props.className}>
+    <label className={props.className}>
+      <img src="/styles/yagi_v_type1.png" alt="yagi_v_type1" width="50" height="50" />
       <input
         type="text"
         value={text}
         placeholder="Please focus..."
         onChange={e => setText(e.target.value)}
       />
-    </div>
+    </label>
   )
 }
 
 export default styled(View)`
-  padding: 40px 16px;
+  display: flex;
+  align-items: center;
+  padding: 40px;
   border: 1px solid #ddd;
+
+  img {
+    margin-right: 16px;
+    transition: all 0.5s;
+    opacity: 0.25;
+  }
+
+  input {
+    margin-bottom: 0;
+  }
 
   &:focus-within {
     border-color: #2c8898;
     background-color: #2c8898;
+
+    img {
+      opacity: 1;
+      transform: rotate(360deg);
+    }
   }
 `
