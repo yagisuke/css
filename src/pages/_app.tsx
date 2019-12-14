aaf61c5193dd8365e1a8e75f75f5cb00a1a41670
yagisuke <yagisuke.0209@gmail.com>
2019-12-14 15:55:55 +0900

    add ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import App from 'next/app'
 import Router from 'next/router'
-const withGA = require('next-ga')
+const withGA = require('next-ga').default
 
 export default withGA('UA-154562499-1', Router)(App)
