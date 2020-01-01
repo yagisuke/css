65eea2e6784fadf2f2912cae5d4743cb219ad0b5
yagisuke <yagisuke.0209@gmail.com>
2020-01-02 01:44:16 +0900

    add custom properties

---
+++ src/pages/styles/houdini_properties_and_values_api.tsx
@@ -0,0 +1,28 @@
+import { Component } from 'react'
+import Head from '~/components/head'
+import { Sample1 } from '~/components/views/styles/houdini_properties_and_values_api'
+
+type Props = {
+  title: string
+  description: string
+}
+
+class Page extends Component<Props> {
+  static async getInitialProps(): Promise<Props> {
+    return {
+      title: 'Properties & Values API',
+      description: 'Properties & Values API Sample.'
+    }
+  }
+  render() {
+    return (
+      <>
+        <Head title={this.props.title} description={this.props.description} />
+        <h1>Properties &amp; Values API</h1>
+        <Sample1 />
+      </>
+    )
+  }
+}
+
+export default Page
