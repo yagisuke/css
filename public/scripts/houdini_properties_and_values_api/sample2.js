14cd8ccfe2a566f7d5f6c76680befcd8f0d206a7
yagisuke <yagisuke.0209@gmail.com>
2020-01-06 01:13:17 +0900

    invalid pattern

--- public/scripts/houdini_properties_and_values_api/sample2.js
+++ public/scripts/houdini_properties_and_values_api/sample2.js
@@ -1,10 +1,11 @@
 if ('registerProperty' in CSS) {
   CSS.registerProperty({
-    name: '--invalid-color',
+    name: '--gradient-start-color',
     syntax: '<color>',
-    initialValue: 'pink',
+    initialValue: 'transparent',
     inherits: false
   });
 } else {
   document.body.innerHTML = 'You need support for <a href="https://drafts.css-houdini.org/css-properties-values-api/">CSS Properties and Values</a> to view this demo :(';
 }
+  
--- src/components/views/styles/houdini_properties_and_values_api/index.tsx
+++ src/components/views/styles/houdini_properties_and_values_api/index.tsx
@@ -1,3 +1,4 @@
 import Sample1 from './sample1'
+import Sample2 from './sample2'
 
-export { Sample1 }
+export { Sample1, Sample2 }
