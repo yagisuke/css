0e308363ed19d625f6b7c7e508bdabb5a2440690
yagisuke <yagisuke.0209@gmail.com>
2020-01-05 20:22:14 +0900

    add Properties & Values API

--- src/components/views/styles/houdini_properties_and_values_api/sample1.tsx
+++ src/components/views/styles/houdini_properties_and_values_api/sample1.tsx
@@ -5,21 +5,28 @@ type Props = {
   className?: string
 }
 
-const View: React.FC<Props> = props => <div className={props.className}>Please hover...</div>
+const View: React.FC<Props> = props => (
+  <>
+    <div className={props.className}>Please hover...</div>
+    <script src="/scripts/houdini_properties_and_values_api/index.js" />
+  </>
+)
 
 export default styled(View)`
-  --gradientStart: #fff;
-  --gradientEnd: #4a4a4a;
-  background: linear-gradient(var(--gradientStart), var(--gradientEnd));
-  color: var(--gradientEnd);
   height: 300px;
   padding: 16px;
   font-size: 20px;
   font-weight: bold;
   box-sizing: border-box;
 
+  --gradient-start: white;
+  --gradient-end: gray;
+  background: linear-gradient(var(--gradient-start), var(--gradient-end));
+  color: var(--gradient-end);
+  transition: --gradient-start 1.5s;
+
   &:hover {
-    --gradientStart: #4a4a4a;
-    --gradientEnd: #fff;
+    --gradient-start: gray;
+    --gradient-end: white;
   }
 `
