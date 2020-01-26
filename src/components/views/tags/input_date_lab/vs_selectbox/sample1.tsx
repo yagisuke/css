2e0a7ea1ea7570372d38a441a792df2a8076d2df
yagisuke <yagisuke.0209@gmail.com>
2020-01-27 03:25:49 +0900

    input date: sample2

--- src/components/views/tags/input_date_lab/vs_selectbox/sample1.tsx
+++ src/components/views/tags/input_date_lab/vs_selectbox/sample1.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import styled from 'styled-components'
 
 type Props = {
@@ -6,6 +6,48 @@ type Props = {
   value: string
 }
 
-const View: React.FC<Props> = props => <div className={props.className}>hoge</div>
+const View: React.FC<Props> = props => {
+  const [date, updateDate] = React.useState(props.value)
+  const [time, setTime] = React.useState({
+    start: 0,
+    end: 0
+  })
 
-export default styled(View)``
+  const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
+    updateDate(event.target.value)
+  }, [])
+
+  const handleStart = useCallback(() => {
+    setTime({ ...time, start: new Date().getTime() })
+  }, [time])
+
+  const handleEnd = useCallback(() => {
+    setTime({ ...time, end: new Date().getTime() })
+  }, [time])
+
+  return (
+    <div className={props.className}>
+      <input
+        type="tel"
+        name="tel"
+        placeholder="1979/01/01"
+        pattern="[0-9０-９/]{10}"
+        value={date}
+        onChange={handleChange}
+        onFocus={handleStart}
+        onBlur={handleEnd}
+      />
+      <p>value: {date || 'none'}</p>
+      <p>time: {time.start && time.end ? Math.floor((time.end - time.start) / 1000) : ' --- '}秒</p>
+    </div>
+  )
+}
+
+export default styled(View)`
+  input {
+    width: 100%;
+    max-width: 300px;
+    height: 50px;
+    font-size: 18px;
+  }
+`
--- src/pages/tags/input_date_lab/vs_selectbox.tsx
+++ src/pages/tags/input_date_lab/vs_selectbox.tsx
@@ -1,6 +1,6 @@
 import { Component } from 'react'
 import Head from '~/components/head'
-import { Sample1 } from '~/components/views/tags/input_date_lab/vs_selectbox'
+import { Sample1, Sample2 } from '~/components/views/tags/input_date_lab/vs_selectbox'
 
 type Props = {
   title: string
@@ -20,7 +20,10 @@ class Page extends Component<Props> {
       <>
         <Head title={this.props.title} description={this.props.description} />
         <h1>{this.props.title}</h1>
+        <h2>{`input[type="tel"]`}</h2>
         <Sample1 value="" />
+        <h2>Selectbox</h2>
+        <Sample2 value="" minAge={8} maxAge={120} />
       </>
     )
   }
